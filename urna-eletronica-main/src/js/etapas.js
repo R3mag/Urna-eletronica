c98191a3b6f0234457f219c09043f8268bd7c598
Arthur Azevedo Barbosa do Carmo <122834504+R3mag@users.noreply.github.com>
2023-01-25 19:57:09 -0300

    Urna Versão 2.0 Atualizado
    Melhoria no estilo

---
+++ urna-eletronica-main/src/js/etapas.js
@@ -0,0 +1,45 @@
+let etapas = [
+  {
+    titulo: "DEPUTADO FEDERAL",
+    numeros: 4,
+    candidatos: [
+      {
+        numero: "1111",
+        nome: "Eneas",
+        partido: "Brasil",
+        fotos: [{ url: "eneas.jpg", legenda: "Deputado Federal" }],
+      },
+      
+    ],
+  },
+  {
+    titulo: "SENADOR",
+    numeros: 3,
+    candidatos: [
+      {
+        numero: "222",
+        nome: "Nikolas Ferreira",
+        partido: "PL",
+        fotos: [{ url: "nik.jpg", legenda: "Senador" }],
+      },
+      
+    ],
+  },
+  {
+    titulo: "PRESIDENTE",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "33",
+        nome: "Getulio Vargas",
+        partido: "Brasil",
+        vice: "Vaso",
+        fotos: [
+          { url: "getulio.jpg", legenda: "Presidente" },
+          { url: "vasco.jpg", legenda: "Vice-Presidente", small: true },
+        ],
+      },
+      
+    ],
+  },
+];
